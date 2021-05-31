c24c3957e1462946c86cfa0dcbb253b5bd572754
hash1018 <46293206+hash1018@users.noreply.github.com>
2021-05-31 16:14:46 +0900

    added icons

--- Ui/FrameWidget.cpp
+++ Ui/FrameWidget.cpp
@@ -6,9 +6,64 @@ FrameWidget::FrameWidget(QWidget* parent)
 	:QWidget(parent),ui(new Ui::FrameWidget) {
 
 	ui->setupUi(this);
+
+
+	ui->restoreButton->hide();
+	ui->neverPinButton->hide();
+	ui->onlyPlayPinButton->hide();
+
+
+	connect(ui->closeButton, &QPushButton::clicked, this, &FrameWidget::closeButtonClicked);
+	connect(ui->maximizeButton, &QPushButton::clicked, this, &FrameWidget::maximizeButtonClicked);
+	connect(ui->minimizeButton, &QPushButton::clicked, this, &FrameWidget::minimizeButtonClicked);
+	connect(ui->restoreButton, &QPushButton::clicked, this, &FrameWidget::restoreButtonClicked);
+	connect(ui->alwaysPinButton, &QPushButton::clicked, this, &FrameWidget::alwaysPinButtonClicked);
+	connect(ui->fullscreenButton, &QPushButton::clicked, this, &FrameWidget::fullscreenButtonClicked);
+	connect(ui->onlyPlayPinButton, &QPushButton::clicked, this, &FrameWidget::onlyPlayPinButtonClicked);
+	connect(ui->neverPinButton, &QPushButton::clicked, this, &FrameWidget::neverPinButtonClicked);
+	connect(ui->settingsButton, &QPushButton::clicked, this, &FrameWidget::settingsButtonClicked);
+
+
 }
 
 FrameWidget::~FrameWidget() {
 
 	delete ui;
+}
+
+
+void FrameWidget::closeButtonClicked() {
+
+}
+
+void FrameWidget::maximizeButtonClicked() {
+
+}
+
+void FrameWidget::minimizeButtonClicked() {
+
+}
+
+void FrameWidget::restoreButtonClicked() {
+
+}
+
+void FrameWidget::alwaysPinButtonClicked() {
+
+}
+
+void FrameWidget::onlyPlayPinButtonClicked() {
+
+}
+
+void FrameWidget::neverPinButtonClicked() {
+
+}
+
+void FrameWidget::settingsButtonClicked() {
+
+}
+
+void FrameWidget::fullscreenButtonClicked() {
+
 }
--- Ui/FrameWidget.h
+++ Ui/FrameWidget.h
@@ -9,12 +9,27 @@ QT_END_NAMESPACE
 
 class FrameWidget : public QWidget {
 
+	Q_OBJECT
+
+
 private:
 	Ui::FrameWidget* ui;
 
 public:
 	FrameWidget(QWidget* parent = nullptr);
 	~FrameWidget();
+
+
+private slots:
+	void closeButtonClicked();
+	void maximizeButtonClicked();
+	void minimizeButtonClicked();
+	void restoreButtonClicked();
+	void alwaysPinButtonClicked();
+	void onlyPlayPinButtonClicked();
+	void neverPinButtonClicked();
+	void settingsButtonClicked();
+	void fullscreenButtonClicked();
 
 };
 
